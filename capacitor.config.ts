652366ee07b0a4b38b0d0c1090374d292e472b88
Roland-fARKER <rolando.loaisiga21908138@estu.unan.edu.ni>
2024-11-19 13:15:53 -0600

    fix ui and add android folder to git ignore

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.solitions.app',
+  appName: 'solutions',
+  webDir: 'dist/app-algebra/browser'
+};
+
+export default config;
